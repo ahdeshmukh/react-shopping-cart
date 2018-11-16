beb76ea1b5f7ef1501854757171bb43a1bf1a908
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2018-11-15 23:30:37 -0500

    text updates

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+		  <h3>Shopping Cart</h3>
           <a
             className="App-link"
             href="https://reactjs.org"
