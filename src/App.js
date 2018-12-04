3ca03f45849d6eda235c35879181de5f4de474db
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2018-12-03 23:13:29 -0500

    test

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React aws s3 travis test - trying cloudfront invalidation - if you see this then it is SUCCESSFUL - trying tags - 1
+            Learn React aws s3 travis test - trying cloudfront invalidation - if you see this then it is SUCCESSFUL - trying tags - 2
           </a>
         </header>
       </div>
