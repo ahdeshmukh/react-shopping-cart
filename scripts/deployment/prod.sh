3fe9d7994e177d6433ff78114100e177e65d4cbb
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2018-12-11 21:25:06 -0500

    trying travis variable in script

--- scripts/deployment/prod.sh
+++ scripts/deployment/prod.sh
@@ -2,6 +2,5 @@
 
 # https://docs.travis-ci.com/user/deployment/script/
 
-sed -i -e 's/REST_API_URL/$PROD_REST_API_URL/g' src/constants/Config.js.sample
-#mv src/constants/Config.js.sample src/constants/Config.js
-ls -la
+sed -i -e 's/REST_API_URL/${PROD_REST_API_URL}/g' src/constants/Config.js.sample
+mv src/constants/Config.js.sample src/constants/Config.js
