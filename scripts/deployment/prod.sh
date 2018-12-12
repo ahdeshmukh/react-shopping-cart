e86d2f64597e1b62cae17189584d4e170248f5b3
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2018-12-11 21:30:02 -0500

    trying travis variable in script - something broke previously

--- scripts/deployment/prod.sh
+++ scripts/deployment/prod.sh
@@ -2,5 +2,5 @@
 
 # https://docs.travis-ci.com/user/deployment/script/
 
-sed -i -e 's/REST_API_URL/${PROD_REST_API_URL}/g' src/constants/Config.js.sample
+sed -i -e 's/REST_API_URL/$PROD_REST_API_URL/g' src/constants/Config.js.sample
 mv src/constants/Config.js.sample src/constants/Config.js
