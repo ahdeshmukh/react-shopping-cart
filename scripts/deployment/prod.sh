90c2a343df9a1f54cf9bd6323466709c634b5406
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2018-12-11 21:03:56 -0500

    trying scripts/deployment/prod.sh

---
+++ scripts/deployment/prod.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# https://docs.travis-ci.com/user/deployment/script/
+
+sed -i -e 's/REST_API_URL/$PROD_REST_API_URL/g' src/constants/Config.js.sample
+mv src/constants/Config.js.sample src/constants/Config.js
