540daf672bbaffeb479fb2b1784407079ce607e2
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2018-12-12 14:47:05 -0500

    test

--- scripts/deployment/prod.sh
+++ scripts/deployment/prod.sh
@@ -2,5 +2,6 @@
 
 # https://docs.travis-ci.com/user/deployment/script/
 
-sed -i -e 's/REST_API_URL/'$PROD_REST_API_URL'/g' src/constants/Config.js.sample
+echo ${PROD_REST_API_URL}
+sed -i -e 's/REST_API_URL/'${PROD_REST_API_URL}'/g' src/constants/Config.js.sample
 mv src/constants/Config.js.sample src/constants/Config.js
