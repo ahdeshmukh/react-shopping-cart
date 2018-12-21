6a2b29a75ebd5ffacab130fbfc39315f19e47ba7
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2018-12-20 21:10:42 -0500

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,11 +8,11 @@ REST_API_URL=${DEV_REST_API_URL}
 
 if [ "$1" == "master" ]; then
 	REST_API_URL=${PROD_REST_API_URL}
-else if [ "$1" == "stage" ]; then
+elif [ "$1" == "stage" ]; then
 	REST_API_URL=${STAGE_REST_API_URL}
 fi
 
-echo 'From shell script '$REST_API_URL
+echo "Rest API URL is "$REST_API_URL
 
 #echo ${PROD_REST_API_URL}
 #sed -i -e 's/REST_API_URL/'$REST_API_URL'/g' src/constants/Config.js.sample
