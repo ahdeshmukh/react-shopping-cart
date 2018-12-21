f6675e530ac7ad3fb341a9b80e497bad2ec32366
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2018-12-20 20:58:20 -0500

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,12 @@
 
 echo "First arg: $1"
 
-REST_API_URL = ${DEV_REST_API_URL}
+REST_API_URL=${DEV_REST_API_URL}
 
 if [ "$1" == "master" ]; then
-	REST_API_URL = ${PROD_REST_API_URL}
+	REST_API_URL=${PROD_REST_API_URL}
 else if [ "$1" == "stage" ]; then
-	REST_API_URL = ${STAGE_REST_API_URL}
+	REST_API_URL=${STAGE_REST_API_URL}
 fi
 
 echo 'From shell script '$REST_API_URL
